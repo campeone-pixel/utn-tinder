d05c7ed7a284feb558ea245dce466fcb40db5fb7
matias <mapcampeone@gmail.com>
2024-04-24 17:04:23 -0300

    agregue el menu del programa

--- utn-tinder.py
+++ utn-tinder.py
@@ -1,13 +1,28 @@
 # Datos de los estudiantes
 estudiante1_email = "estudiante1@ayed.com"
 estudiante1_contraseña = "111222"
+estudiante1_nombre = "111222"
+estudiante1_fecha_nac = "111222"
+estudiante1_biografía = []
+estudiante1_hobbies= []
+
 estudiante2_email = "estudiante2@ayed.com"
 estudiante2_contraseña = "333444"
+estudiante2_nombre = "111222"
+estudiante2_fecha_nac = "111222"
+estudiante2_biografía = []
+estudiante2_hobbies= []
+
 estudiante3_email = "estudiante3@ayed.com"
 estudiante3_contraseña = "555666"
+estudiante3_nombre = "111222"
+estudiante3_fecha_nac = "111222"
+estudiante3_biografía = []
+estudiante3_hobbies= []
 
-# Función para el inicio de sesión
+
 def login():
+   
     intentos = 3
     while intentos > 0:
         email = input("Ingrese su email: ")
@@ -16,13 +31,61 @@ def login():
         if (email == estudiante1_email and contraseña == estudiante1_contraseña) or \
            (email == estudiante2_email and contraseña == estudiante2_contraseña) or \
            (email == estudiante3_email and contraseña == estudiante3_contraseña):
-            print("Inicio de sesión exitoso.")
+            
+            
+            estudiante_conectado = True
+            email_estudiante_conectado = email
+            
             return
         else:
             intentos -= 1
             print("Email o contraseña incorrectos. Intentos restantes:", intentos)
-    
-    print("Ha excedido el número de intentos. Programa cerrado.")
+    return estudiante_conectado,email_estudiante_conectado 
 
-# Iniciar sesión
-login()
+def editar_datos_personales():
+    pass
+
+def ver_candidatos():
+    pass
+
+
+def main():
+    print("Bienvenido a UTN Tinder.")
+
+    estudiante_conectado,email_estudiante_conectado = login()
+
+    if estudiante_conectado:
+        print("inicio exitoso")
+
+        # Imprimir el menú
+        print("1. Gestionar mi perfil")
+        print("2. Gestionar candidatos")
+        print("3. Matcheos")
+        print("4. Reportes estadísticos")
+        print("0. Salir")
+
+        
+        opcion = input("Por favor, seleccione una opción: ")
+
+      
+        if opcion == "1":
+            editar_datos_personales()
+        elif opcion == "2":
+            ver_candidatos()
+        elif opcion == "3":
+            print("En construccion")
+        elif opcion == "4":
+            print("En construccion")
+        elif opcion == "0":
+            print("En construccion")
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+    else:
+        print("credenciales invalidas")
+
+
+
+
+
+
+main()
