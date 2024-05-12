eac3a6379435723f3984a0a5bad3b8615214a037
matias <mapcampeone@gmail.com>
2024-05-12 18:33:18 -0300

    realizamos los modulos de perfil y de gestion de candidatos

---
+++ prueba.py
@@ -0,0 +1,46 @@
+""" cp = 0
+sp = 0
+n = 0
+sn = 0
+
+for x in range(6):
+    numero = int(input("numero:"))
+    if numero > 0:
+        cp = cp + 1
+        sp = sp + numero
+        p = sp / cp
+    else:
+        if numero == 0:
+            n = n + 1
+        else:
+            sn = sn + numero
+print(
+    "promedio valores positivo: ", p, "suma de negativos: ", sn, "cantidad nulos: ", n
+)
+"""
+
+t_hs_p = 0
+t_hs_s = 0
+cs = 0
+cp = 0
+planta = input("planta:")
+
+while planta != "*":
+    seccion = int(input("seccion:"))
+
+    while seccion != 0:
+        hs = int(input("horas"))
+        while hs != 0:
+            t_hs_p = t_hs_p + hs
+            t_hs_s = t_hs_s + hs
+            cs = cs + 1
+            cp = cp + 1
+            hs = int(input("horas"))
+        print("seccion: ", seccion, "total hs: ", t_hs_s, "total empleados: ", cs)
+        cs = 0
+        t_hs_s = 0
+        seccion = int(input("seccion:"))
+    print("planta: ", planta, "total hs: ", t_hs_p, "total empleados: ", cp)
+    cp = 0
+    t_hs_p = 0
+    planta = input("planta:")
--- utn-tinder.py
+++ utn-tinder.py
@@ -1,25 +1,33 @@
 import getpass
+from datetime import datetime
 
-estudiante1_email = "mati"
-estudiante1_contrasenia = "1"
-estudiante1_nombre = "111222"
-estudiante1_fecha_nac = "111222"
-estudiante1_biografia = []
-estudiante1_hobbies = []
+estudiante1_email = "estudiante1@ayed.com"
+estudiante1_contrasenia = "111222"
+estudiante1_nombre = "leonardo"
+estudiante1_fecha_nac = "2000/08/16"
+estudiante1_biografia = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+estudiante1_hobbies = "estudio humanidades y me gusta el pensamiento lateral"
 
 estudiante2_email = "estudiante2@ayed.com"
 estudiante2_contrasenia = "333444"
-estudiante2_nombre = "111222"
-estudiante2_fecha_nac = "111222"
-estudiante2_biografia = []
-estudiante2_hobbies = []
+estudiante2_nombre = "julian"
+estudiante2_fecha_nac = "1987/07/11"
+estudiante2_biografia = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+estudiante2_hobbies = "estudiar matematicas y hacer muchos calculos de integrales"
 
 estudiante3_email = "estudiante3@ayed.com"
 estudiante3_contrasenia = "555666"
-estudiante3_nombre = "111222"
-estudiante3_fecha_nac = "111222"
-estudiante3_biografia = []
-estudiante3_hobbies = []
+estudiante3_nombre = "carolina"
+estudiante3_fecha_nac = "2002/08/16"
+estudiante3_biografia = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+estudiante3_hobbies = "surf, andar en bici, estudiar fisica"
+
+
+def calcular_edad(fecha_nacimiento):
+    fecha_nacimiento = datetime.strptime(fecha_nacimiento, "%Y/%m/%d")
+    dias_anios = 365.2425
+    age = int((datetime.today() - fecha_nacimiento).days / dias_anios)
+    return age
 
 
 def menu_principal():
@@ -47,34 +55,53 @@ def login():
             or (email == estudiante3_email and contraseña == estudiante3_contrasenia)
         ):
             print("inicio exitoso")
-            return True  # ahora  es true
+            return True, email  # ahora  es true
         elif intentos != 1:
             intentos = intentos - 1
             print("intente nuevamente")
             print("intentos restantes:", intentos)
         else:
             print("credenciales invalidas\n")
-            return False  # ahora  es false
+            return False, None  # ahora  es false
 
 
-def gestionar_perfil():
+def gestionar_perfil(estudiante):
     print("1. Gestionar mi perfil")
+    print("2. Eliminar mi perfil")
     print("0. Volver")
 
-    """
-    Gestionar mi perfil
-    a. Editar mis datos personales
-    b. Eliminar mi perfil
-    c. Volver
-    """
     opcion = int(input("Por favor, seleccione una opción: "))
 
     while opcion != 0:
         if opcion == 1:
-            print(
-                "ya gestionamos tu perfil de ",
-                "\n",
-            )
+
+            if estudiante == "estudiante1@ayed.com":
+                print("Fecha de Nacimiento actual: ", estudiante1_fecha_nac)
+                estudiante1_fecha_nac = input("Ingrese nueva fecha nacimiento: ")
+                print("Hobbies actuales: ", estudiante1_hobbies)
+                estudiante1_hobbies = input("Ingrese nuevos hobbies: ")
+                print("Biografia actual: ", estudiante1_biografia)
+                estudiante1_biografia = input("Ingrese nueva biografia: ")
+            elif estudiante == "estudiante2@ayed.com":
+                print("Fecha de Nacimiento actual: ", estudiante2_fecha_nac)
+                estudiante2_fecha_nac = input("Ingrese nueva fecha nacimiento: ")
+                print("Hobbies actuales: ", estudiante2_hobbies)
+                estudiante2_hobbies = input("Ingrese nuevos hobbies: ")
+                print("Biografia actual: ", estudiante2_biografia)
+                estudiante2_biografia = input("Ingrese nueva biografia: ")
+            elif estudiante == "estudiante3@ayed.com":
+                print("Fecha de Nacimiento actual: ", estudiante3_fecha_nac)
+                estudiante3_fecha_nac = input("Ingrese nueva fecha nacimiento: ")
+                print("Hobbies actuales: ", estudiante3_hobbies)
+                estudiante3_hobbies = input("Ingrese nuevos hobbies: ")
+                print("Biografia actual: ", estudiante3_biografia)
+                estudiante3_biografia = input("Ingrese nueva biografia: ")
+            else:
+                print("no hay ningun estudiante conectado")
+
+        elif opcion == 2:
+            print("eliminando perfil\n")
+
         elif opcion == 0:
             print("Volviendo\n")
         else:
@@ -84,26 +111,61 @@ def gestionar_perfil():
         opcion = int(input("Por favor, seleccione una opción:"))
 
 
-def gestionar_candidatos():
+def gestionar_candidatos(estudiante):
     print("1. Gestionar candidatos")
+    print("2. Reportar un candidato")
     print("0. Volver")
     opcion = int(input("Por favor, seleccione una opción: "))
 
-    """
-    2. Gestionar candidatos
-        a. Ver candidatos
-        b. Reportar un candidato
-        c. Volver
-    """
-
     while opcion != 0:
+
+        def todos_estudiantes():
+            print("Datos del Estudiante:")
+            print("---------------------")
+            print("Email:", estudiante1_email)
+            print("Contraseña:", estudiante1_contrasenia)
+            print("Nombre:", estudiante1_nombre)
+            print("Edad:", calcular_edad(estudiante1_fecha_nac))
+            print("Biografía:")
+            print(estudiante1_biografia)
+            print("Hobbies:", estudiante1_hobbies)
+            print("\n")
+            print("Datos del Estudiante 2:")
+            print("------------------------")
+            print("Email:", estudiante2_email)
+            print("Contraseña:", estudiante2_contrasenia)
+            print("Nombre:", estudiante2_nombre)
+            print("Edad:", calcular_edad(estudiante2_fecha_nac))
+            print("Biografía:")
+            print(estudiante2_biografia)
+            print("Hobbies:", estudiante2_hobbies)
+            print("\n")
+            print("Datos del Estudiante 3:")
+            print("------------------------")
+            print("Email:", estudiante3_email)
+            print("Contraseña:", estudiante3_contrasenia)
+            print("Nombre:", estudiante3_nombre)
+            print("Edad:", calcular_edad(estudiante3_fecha_nac))
+            print("Biografía:")
+            print(estudiante3_biografia)
+            print("Hobbies:", estudiante3_hobbies)
+            print("\n")
+
         if opcion == 1:
-            print(
-                "ya gestionamos los candidatos de ",
-                "\n",
-            )
+            todos_estudiantes()
+
+            me_gusta = input("Ingrese nombre estudiante")
+            while (
+                me_gusta != estudiante1_nombre
+                or me_gusta != estudiante2_nombre
+                or me_gusta != estudiante3_nombre
+            ):
+                print("No existe estudiante con ese nombre")
+                todos_estudiantes()
+                me_gusta = input("Ingrese estudiante nuevamente")
+
         elif opcion == 0:
-            print("Volviendo\n")
+            print("Candidato reportado\n")
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.\n")
         print("1. Gestionar candidatos")
@@ -112,22 +174,22 @@ def gestionar_candidatos():
 
 
 def main():
-    estudiante_conectado = login()
+    conectado, estudiante = login()
 
-    while estudiante_conectado:
+    while conectado:
         opcion = menu_principal()
         while opcion != 5:
             if opcion == 1:
-                gestionar_perfil()
+                gestionar_perfil(estudiante)
             elif opcion == 2:
-                gestionar_candidatos()
+                gestionar_candidatos(estudiante)
             elif opcion == 3:
                 print("En construccion\n")
             elif opcion == 4:
                 print("En construccion\n")
         print("Saliendo")
         print("-----------------------------------------------------------------------")
-        estudiante_conectado = False
+        conectado = False
 
 
 main()
